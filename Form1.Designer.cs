a8c77cd09b32e79738f364513f6a136a9e4a57f5
den1k0 <danielneustus.777@gmail.com>
2024-01-07 01:42:21 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Youtube_Download
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.download = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(547, 73);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(94, 29);
+            this.download.TabIndex = 0;
+            this.download.Text = "Download";
+            this.download.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Скачать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(417, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Вставьте ссылку сюда ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(488, 237);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 185);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(488, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(206, 134);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(29, 324);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(200, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Назначить путь к файлу ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(29, 363);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(417, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(356, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Вставить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(29, 294);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(125, 24);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Только аудио";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(29, 264);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(147, 24);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Низкое качество";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(697, 434);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button download;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private CheckBox checkBox1;
+        private TextBox textBox2;
+        private Button button2;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using YoutubeDLSharp;
+using YoutubeDLSharp.Metadata;
+using YoutubeDLSharp.Options;
+
+namespace Youtube_Download
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            TryDownload();
+        }
+        async void TryDownload()
+        {
+            pictureBox1.Show();
+            int downloadable = 1;
+            var ytdl = new YoutubeDL();
+            if (File.Exists("yt-dlp.exe") && File.Exists("ffmpeg.exe"))
+            {
+                ytdl.YoutubeDLPath = $"";
+                ytdl.FFmpegPath = $"";
+            }
+            else
+            {
+                await YoutubeDLSharp.Utils.DownloadYtDlp();
+                await YoutubeDLSharp.Utils.DownloadFFmpeg();
+            }
+            if (textBox2.Enabled)
+            {
+                if (Directory.Exists(textBox2.Text))
+                {
+                    downloadable = 1;
+                }
+                else
+                {
+                    downloadable = 0;
+                }
+            }
+            if (downloadable == 1)
+            {
+                Download();
+            }
+            if (downloadable == 0)
+            {
+                MessageBox.Show("Неверный путь к файлу или неверная ссылка");
+            }
+        }
+        int checker;
+        int checker2;
+        int checker3;
+        async void Download()
+        {
+            var options = new OptionSet()
+            {
+                Format = "best",
+            };
+
+            if (checker3 % 2 == 0)
+            {
+                options.Format = "best";
+                options.AudioQuality = 0;
+            }
+            else
+            {
+                options.Format = "worst";
+                options.AudioQuality = 7;
+            }
+            
+            var ytdl = new YoutubeDL();
+            ytdl.OutputFolder = $"{textBox2.Text}";
+            if (checker2 % 2 == 0)
+            {
+                options.RecodeVideo = VideoRecodeFormat.Mp4;
+                var res = await ytdl.RunVideoDownload(textBox1.Text, overrideOptions: options);
+            }
+            else
+            {
+                options.AudioFormat = AudioConversionFormat.Mp3;
+                var res = await ytdl.RunAudioDownload(textBox1.Text, overrideOptions: options);
+            }
+            
+            pictureBox1.Hide();
+            if (checker % 2 == 0)
+            {
+                MessageBox.Show("Загрузка завершена, файл находится там где приложение.");
+            }
+            else
+            {
+                MessageBox.Show($"Загрузка завершена в папку {textBox2.Text}");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checker % 2 == 0)
+            {
+                textBox2.Enabled = true;
+                textBox2.Text = "C:\\";
+            }
+            else
+            { 
+            textBox2.Enabled = false;
+            textBox2.Text = "";
+            }
+            checker++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Clipboard.GetText();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checker2++;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            checker3++;
+        }
+    }
+}
